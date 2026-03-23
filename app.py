2a4945e42d6ff1cbceb0c7c110d4ae8cfc0fcdda
Gowsika Annadurai <149564937+gowsikacs@users.noreply.github.com>
2026-03-23 10:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,7 @@ Skin Type: {skin_type}
     return result
 
 def beauty_recommendation(acne, wrinkle, dark_circle, hair, skin_type):
-
-rec = []
+  rec = []
 
     if "Moderate" in acne or "Severe" in acne:
         rec.append("Use Salicylic Acid Cleanser")
